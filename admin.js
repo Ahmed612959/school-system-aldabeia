312d6553f3cb53beeef33b9f307896db5df48152
Ahmed612959 <am3560380@gmail.com>
2025-09-16 22:48:47 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     async function getFromServer(endpoint) {
         try {
-            const response = await fetch(`http://localhost:3000${endpoint}`);
+           const response = await fetch(`https://school-system-aldabeia-production-31f7.up.railway.app${endpoint}`);
             if (!response.ok) throw new Error(`خطأ ${response.status}`);
             const data = await response.json();
             console.log(`Data loaded from server for ${endpoint}:`, data.length, 'items');
