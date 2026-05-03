5ffd26f13880fbd5e55deae0f57792d8c5bc9dfc
Ahmed612959 <am3560380@gmail.com>
2026-05-03 13:38:26 +0300

    Update signup.js

--- public/signup.js
+++ public/signup.js
@@ -29,15 +29,7 @@ async function checkUsernameAvailability(username) {
         const res = await fetch('/api/check-username', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-    fullName,
-    username,
-    password,
-    studentCode, // 👈 بدل id
-    phone,
-    parentName,
-    parentId
-})
+            body: JSON.stringify({ username }) // ✅ الصح
         });
 
         const data = await res.json();
