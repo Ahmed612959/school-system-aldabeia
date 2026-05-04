7d9da9e1e610a9232d5729865951e50fc55f20c4
Ahmed612959 <am3560380@gmail.com>
2026-05-04 21:18:38 +0300

    Update server.js

--- public/server.js
+++ public/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const crypto = require('crypto');
+const serverless = require('serverless-http');
 const app = express();
 
 app.use(cors());
