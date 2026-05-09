27653c1a5229ab36da4bfc1d515ff643a7ca7b1f
Ahmed612959 <am3560380@gmail.com>
2026-05-09 13:32:22 -0700

    Create test.js

---
+++ api/test.js
@@ -0,0 +1,8 @@
+module.exports = (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json({
+        status: 'ok',
+        message: 'API is working!',
+        timestamp: new Date().toISOString()
+    });
+};
