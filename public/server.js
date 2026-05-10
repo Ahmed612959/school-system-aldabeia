7eda5f20de1d9756145f3ca64914faee0f939571
Ahmed612959 <am3560380@gmail.com>
2026-05-10 13:12:07 -0700

    Update server.js

--- public/server.js
+++ public/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const crypto = require('crypto');
+const bcrypt = require('bcryptjs');   // ← بدل bcrypt العادي
 const pdfParse = require('pdf-parse');
 
 const app = express();
